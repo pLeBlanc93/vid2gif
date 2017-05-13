734983f0b308aef517a8e7dc5e921f4e4e4e5fdd
Paul LeBlanc <PLeBlanc@esri.com>
2017-05-13 15:40:48 -0700

    moar comments

--- vid2gif.bat
+++ vid2gif.bat
@@ -3,7 +3,7 @@ REM since GIFs can only use 256 colors, ffmpeg analyzes the image to determine t
 REM an output palette image is created in memory and passed to a separate ffmpeg process during conversion
 REM see http://blog.pkh.me/p/21-high-quality-gif-with-ffmpeg.html
 
-REM use ffmpeg if it's available
+REM check if ffmpeg is present, download from zernoe if not.
 SET ffmpeg="%~dp0\ffmpeg-3.2.4-win64-static\bin\ffmpeg.exe"
 SET zip_file="%appdata%\ffmpeg.zip"
 If NOT EXIST %ffmpeg% (
@@ -13,12 +13,14 @@ If NOT EXIST %ffmpeg% (
     curl http://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-3.2.4-win64-static.zip -o %zip_file%
     unzip %zip_file% *ffmpeg.exe -d "%~dp0."
 )
+
+REM need to set this so variables can be set inside if statement
 Setlocal EnableDelayedExpansion
 
 REM default values
 SET s=0
 SET e=9999
-REM set to 1 to prompt for start/end time of video.. by default, convert entire file
+REM set to 1 to prompt for start/end time of video.. by default, converts entire file
 IF 0 == 1 (
     SET /p s="Enter start time in seconds s.S (default is start of video)"
     SET /p e="Enter end time in seconds s.S (default is end of video)"
