667c2ef2ad0f88a02820431a993e359c9b018d32
Paul LeBlanc <PLeBlanc@esri.com>
2017-05-13 15:21:59 -0700

    auto-download
    download ffmpeg if it can't be found
    add support for spaces and drag/drop from different drives

--- vid2gif.bat
+++ vid2gif.bat
@@ -2,6 +2,17 @@
 REM since GIFs can only use 256 colors, ffmpeg analyzes the image to determine the best 256 colors to use
 REM an output palette image is created in memory and passed to a separate ffmpeg process during conversion
 REM see http://blog.pkh.me/p/21-high-quality-gif-with-ffmpeg.html
+pause
+REM use ffmpeg if it's available
+SET ffmpeg="%~dp0\ffmpeg-3.2.4-win64-static\bin\ffmpeg.exe"
+SET zip_file="%appdata%\ffmpeg.zip"
+If NOT EXIST %ffmpeg% (
+    ECHO.
+    ECHO ffmpeg cannot be found, downloading...
+    ECHO.
+    curl http://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-3.2.4-win64-static.zip -o %zip_file%
+    unzip %zip_file% *ffmpeg.exe -d "%~dp0."
+)
 Setlocal EnableDelayedExpansion
 
 REM default values
@@ -13,6 +24,6 @@ IF 0 == 1 (
     SET /p e="Enter end time in seconds s.S (default is end of video)"
 )
 
-.\bin\ffmpeg.exe -y -i "%~1" -vf fps=20,scale=iw:-1:flags=lanczos,palettegen -f image2pipe -vcodec png - | ^
-.\bin\ffmpeg.exe -y -i "%~1" -i - -ss !s! -to !e! -filter_complex "fps=20,scale=iw:-1:flags=lanczos[x];[x][1:v]paletteuse" "%~n1.gif"
+!ffmpeg! -y -i %1 -vf fps=20,scale=iw:-1:flags=lanczos,palettegen -f image2pipe -vcodec png - | ^
+!ffmpeg! -y -i %1 -i - -ss !s! -to !e! -filter_complex "fps=20,scale=iw:-1:flags=lanczos[x];[x][1:v]paletteuse" "%~dpn1.gif"
 PAUSE
