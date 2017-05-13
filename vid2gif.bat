f1520ad959518333f66af6e9f24d004e5428afe0
Paul LeBlanc <PLeBlanc@esri.com>
2017-05-13 13:37:48 -0700

    optional prompt
    be default, don't ask for start/end time

--- vid2gif.bat
+++ vid2gif.bat
@@ -2,13 +2,17 @@
 REM since GIFs can only use 256 colors, ffmpeg analyzes the image to determine the best 256 colors to use
 REM an output palette image is created in memory and passed to a separate ffmpeg process during conversion
 REM see http://blog.pkh.me/p/21-high-quality-gif-with-ffmpeg.html
+Setlocal EnableDelayedExpansion
 
 REM default values
-set s=0
-set e=9999
-SET /p s= Enter start time in seconds s.S (default is start of video)
-SET /p e= Enter end time in seconds s.S (default is end of video)
+SET s=0
+SET e=9999
+REM set to 1 to prompt for start/end time of video.. by default, convert entire file
+IF 0 == 1 (
+    SET /p s="Enter start time in seconds s.S (default is start of video)"
+    SET /p e="Enter end time in seconds s.S (default is end of video)"
+)
 
 .\bin\ffmpeg.exe -y -i "%~1" -vf fps=20,scale=iw:-1:flags=lanczos,palettegen -f image2pipe -vcodec png - | ^
-.\bin\ffmpeg.exe -y -i "%~1" -i - -ss %s% -to %e% -filter_complex "fps=20,scale=iw:-1:flags=lanczos[x];[x][1:v]paletteuse" "%~n1.gif"
-pause
+.\bin\ffmpeg.exe -y -i "%~1" -i - -ss !s! -to !e! -filter_complex "fps=20,scale=iw:-1:flags=lanczos[x];[x][1:v]paletteuse" "%~n1.gif"
+PAUSE
