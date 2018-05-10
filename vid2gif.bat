16d5a53be31cd92efbed47937c1669b8aac4e324
Paul LeBlanc <PLeBlanc@esri.com>
2018-05-09 19:25:26 -0700

    better time extraction
    much faster for longer videos

--- vid2gif.bat
+++ vid2gif.bat
@@ -3,7 +3,7 @@ REM since GIFs can only use 256 colors, ffmpeg analyzes the image to determine t
 REM an output palette image is created in memory and passed to a separate ffmpeg process during conversion
 REM see http://blog.pkh.me/p/21-high-quality-gif-with-ffmpeg.html
 
-REM check if ffmpeg is present, download from zernoe if not.
+REM check if ffmpeg is present, download from zeranoe if not.
 SET ffmpeg="%~dp0\ffmpeg-4.0-win64-static\bin\ffmpeg.exe"
 SET zip_file="%appdata%\ffmpeg.zip"
 If NOT EXIST %ffmpeg% (
@@ -19,14 +19,15 @@ REM need to set this so variables can be set inside if statement
 Setlocal EnableDelayedExpansion
 
 REM default values
-SET s=0
-SET e=9999
+SET start=0
+SET end=9999
 REM set to 1 to prompt for start/end time of video.. by default, converts entire file
-IF 0 == 1 (
-    SET /p s="Enter start time in seconds s.S (default is start of video)"
-    SET /p e="Enter end time in seconds s.S (default is end of video)"
+IF 1 == 1 (
+    SET /p start="Enter start time in seconds s.S (default is start of video)"
+    SET /p end="Enter end time in seconds s.S (default is end of video)"
 )
 
-!ffmpeg! -y -i %1 -vf fps=20,scale=iw:-1:flags=lanczos,palettegen -f image2pipe -vcodec png - | ^
-!ffmpeg! -y -i %1 -i - -ss !s! -to !e! -filter_complex "fps=20,scale=iw:-1:flags=lanczos[x];[x][1:v]paletteuse" "%~dpn1.gif"
+SET filters="fps=20,scale=iw:-1:flags=lanczos"
+!ffmpeg! -y -ss !start! -to !end! -i %1 -vf !filters!,palettegen -f image2pipe -vcodec png - | ^
+!ffmpeg! -y -ss !start! -to !end! -i %1 -i - -filter_complex "!filters! [x]; [x][1:v] paletteuse" "%~dpn1.gif"
 PAUSE
